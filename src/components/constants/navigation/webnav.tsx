896c825bad025f289fb44b2e8a42805d54d42f13
Jorris-N <nyangejorris@gmail.com>
2023-09-21 14:23:01 +0300

    navigation section completed

---
+++ src/components/constants/navigation/webnav.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Image from 'next/image';
+
+
+export default function WebNav() {
+
+    const router = useRouter ();
+
+    const navigation = {
+        links: [
+            {
+                id: 1,
+                name: 'home',
+                href: '/'
+            },
+            {
+                id: 2,
+                name: 'about',
+                href: '/about'
+            },
+            {
+                id: 3,
+                name:'homestays',
+                href: '/services'
+            },
+            {
+                id: 4,
+                name: 'airbnb',
+                href: '/airbnb'
+            },
+            {
+                id: 5,
+                name: 'blog',
+                href: '/blog'
+            },
+            {
+                id: 6,
+                name: 'contacts',
+                href: '/contacts'
+            }
+        ],
+
+
+    }
+
+  return (
+    <div className='bg-white rounded-full'>
+        <div className='py-6 lg:flex justify-between items-center lg:w-10/12 px-6 mx-auto hidden'>
+        <div className='items-center'>
+                <Link href={'/'} className='flex flex-col items-center'>
+                    {/* <Image src={Logo} alt="" width={70} height={70}/> */}
+
+                </Link>
+
+            </div>
+            <div>
+                <nav className='flex items-center space-x-8 items-center'>
+                    {navigation.links.map((item)=>(
+                        <Link href={item.href} key={item.id} className={router.pathname == item.href ? "text-[#ff3f3f] capitalize font-medium" : "hover:text-[#ff3f3f] ease-in-out duration-300 text-[#202B5D] capitalize font-medium"}>{item.name}</Link>
+                    ))}
+                </nav>
+            </div>
+        </div>
+    </div>
+  )
+}
