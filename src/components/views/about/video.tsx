75809f2de5e10dc78f653abdb4d9643cecb8680b
Jorris-N <nyangejorris@gmail.com>
2023-09-26 10:36:18 +0300

    video section added

---
+++ src/components/views/about/video.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player'
+
+export default function Video() {
+
+    const [showPlayer, setShowPlayer] = useState(false);
+
+    useEffect(() => {
+      setShowPlayer(true);
+    }, []);
+
+  return (
+    <div className='lg:w-10/12 mx-auto px-6 py-16'>
+    <div className='text-center max-w-2xl mx-auto pb-8 space-y-4'>
+        <div className=''>
+            <p className='text-[#ff3f3f] text-lg font-semibold'>
+                Explore our Experience
+            </p>
+        </div>
+        <h1 className='text-4xl font-bold text-neutral-700'>
+            Watch how a day is at one of our resorts
+        </h1>
+      </div>
+      <div className='w-full items-center'>
+        {showPlayer && <ReactPlayer url="/videos/myvideo.mp4" controls playing autoPlay muted volume={1} loop pip width='100%' height='100%'/>}
+      </div>
+    </div>
+  )
+}
